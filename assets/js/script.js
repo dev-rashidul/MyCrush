d872a15b2a66a62fbd1da2ea4d2cae41fa5cba02
dev-rashidul <rashidul.contact@gmail.com>
2023-05-29 00:39:20 +0600

    Homepage done

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,32 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 100)
 
 })
+
+// Slider JavaScript
+
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+      2000: {
+        items: 2,
+      },
+    },
+    navText: [
+      '<i class="fa-solid fa-arrow-left"></i>',
+      '<i class="fa-solid fa-arrow-right"></i>',
+    ],
+  });
